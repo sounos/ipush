b255b97e6956b0265740074ebe4c8046a3ab274c
root <redor@126.com>
2013-10-09 11:41:57 +0800

    upgrade code verstion to 0.0.2

--- config.h
+++ config.h
@@ -126,7 +126,7 @@
 #define PACKAGE_NAME "ipush"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "ipush 0.0.1"
+#define PACKAGE_STRING "ipush 0.0.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "ipush"
@@ -135,7 +135,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.0.1"
+#define PACKAGE_VERSION "0.0.2"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
@@ -144,7 +144,7 @@
 #define TIME_WITH_SYS_TIME 1
 
 /* Version number of package */
-#define VERSION "0.0.1"
+#define VERSION "0.0.2"
 
 /* Define to `long int' if <sys/types.h> does not define. */
 /* #undef off_t */
